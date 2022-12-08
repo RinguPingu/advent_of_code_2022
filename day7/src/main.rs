b69da6059623ce0fc3345cb9b375c2ef4c7e368e
RinguPingu <love.arvidsson@gmail.com>
2022-12-08 09:40:16 +0100

    Solve Day 7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -4,16 +4,32 @@ pub trait FileSystemEntity {
     fn is_dir(&self) -> bool;
     fn is_file(&self) -> bool;
 
+    fn add_directory(&mut self, dir: Directory);
+    fn add_file(&mut self, file: File);
+
     fn get_name(&self) -> &String;
     fn get_size(&self) -> usize;
-    fn get_contents(&mut self) -> Option<&RefCell<Vec<Rc<dyn FileSystemEntity>>>>;
-    fn get_parent(&self) -> Option<&Rc<dyn FileSystemEntity>>;
+    fn get_contents(&self) -> Option<&Vec<Rc<RefCell<dyn FileSystemEntity>>>>;
+    fn get_parent(&self) -> Option<&Rc<RefCell<dyn FileSystemEntity>>>;
+    fn get_child(&self, name: &str) -> Option<&Rc<RefCell<dyn FileSystemEntity>>>;
+
+    fn get_dir_sizes(&self, threshold: usize) -> Option<Vec<(String, usize)>>;
 }
 
-struct File {
+pub struct File {
     name: String,
-    parent: Option<Rc<dyn FileSystemEntity>>,
+    parent: Option<Rc<RefCell<dyn FileSystemEntity>>>,
     size: usize,
+}
+
+impl File {
+    fn new(name: String, parent: Option<Rc<RefCell<dyn FileSystemEntity>>>, size: usize) -> File {
+        File {
+            name: name,
+            parent: parent,
+            size: size,
+        }
+    }
 }
 
 impl FileSystemEntity for File {
@@ -25,6 +41,9 @@ impl FileSystemEntity for File {
         true
     }
 
+    fn add_directory(&mut self, _dir: Directory) {}
+    fn add_file(&mut self, _file: File) {}
+
     fn get_name(&self) -> &String {
         &self.name
     }
@@ -33,37 +52,45 @@ impl FileSystemEntity for File {
         self.size
     }
 
-    fn get_contents(&mut self) -> Option<&RefCell<Vec<Rc<dyn FileSystemEntity>>>> {
+    fn get_contents(&self) -> Option<&Vec<Rc<RefCell<dyn FileSystemEntity>>>> {
         None
     }
 
-    fn get_parent(&self) -> Option<&Rc<dyn FileSystemEntity>> {
+    fn get_parent(&self) -> Option<&Rc<RefCell<dyn FileSystemEntity>>> {
         if let Some(p) = &self.parent {
             Some(p)
         } else {
             None
         }
     }
+
+    fn get_child(&self, _name: &str) -> Option<&Rc<RefCell<dyn FileSystemEntity>>> {
+        None
+    }
+
+    fn get_dir_sizes(&self, _threshold: usize) -> Option<Vec<(String, usize)>> {
+        None
+    }
 }
 
-struct Directory {
+pub struct Directory {
     name: String,
-    parent: Option<Rc<dyn FileSystemEntity>>,
-    contents: RefCell<Vec<Rc<dyn FileSystemEntity>>>,
+    parent: Option<Rc<RefCell<dyn FileSystemEntity>>>,
+    contents: Vec<Rc<RefCell<dyn FileSystemEntity>>>,
 }
 
 impl Directory {
-    fn new(name: String, parent: Option<Rc<dyn FileSystemEntity>>, contents: RefCell<Vec<Rc<dyn FileSystemEntity>>>) -> Directory {
+    fn new(
+        name: String,
+        parent: Option<Rc<RefCell<dyn FileSystemEntity>>>,
+        contents: Vec<Rc<RefCell<dyn FileSystemEntity>>>,
+    ) -> Directory {
         Directory {
             name: name,
             parent: parent,
-            contents: contents
+            contents: contents,
         }
     }
-
-    // fn add_directory(&mut self, dir: Directory) {
-    //     self.contents
-    // }
 }
 
 impl FileSystemEntity for Directory {
@@ -75,6 +102,14 @@ impl FileSystemEntity for Directory {
         false
     }
 
+    fn add_directory(&mut self, dir: Directory) {
+        self.contents.push(Rc::new(RefCell::new(dir)));
+    }
+
+    fn add_file(&mut self, file: File) {
+        self.contents.push(Rc::new(RefCell::new(file)));
+    }
+
     fn get_name(&self) -> &String {
         &self.name
     }
@@ -82,68 +117,176 @@ impl FileSystemEntity for Directory {
     fn get_size(&self) -> usize {
         let mut sum: usize = 0;
 
-        for entity in self.contents.borrow().iter() {
-            sum += entity.get_size();
+        for entity in &self.contents {
+            sum += entity.borrow().get_size();
         }
 
         return sum;
     }
 
-    fn get_contents(&mut self) -> Option<&RefCell<Vec<Rc<dyn FileSystemEntity>>>> {
+    fn get_contents(&self) -> Option<&Vec<Rc<RefCell<dyn FileSystemEntity>>>> {
         Some(&self.contents)
     }
 
-    fn get_parent(&self) -> Option<&Rc<dyn FileSystemEntity>> {
+    fn get_parent(&self) -> Option<&Rc<RefCell<dyn FileSystemEntity>>> {
         if let Some(p) = &self.parent {
             Some(p)
         } else {
             None
         }
     }
+
+    fn get_child(&self, name: &str) -> Option<&Rc<RefCell<dyn FileSystemEntity>>> {
+        self.contents
+            .iter()
+            .find(|&d| d.borrow().get_name() == name)
+    }
+
+    fn get_dir_sizes(&self, threshold: usize) -> Option<Vec<(String, usize)>> {
+        let mut sizes: Vec<(String, usize)> = Vec::new();
+
+        for entity in &self.contents {
+            if entity.borrow().is_dir() {
+                let name = entity.borrow().get_name().to_string();
+                let size = entity.borrow().get_size();
+                
+                // Part 1 Solution
+                // if size <= threshold {
+
+                // Part 2 Solution
+                if size >= threshold {
+                    sizes.push((name, size));
+                }
+
+                if !entity.borrow().get_contents().unwrap().is_empty() {
+                    let mut more_sizes = entity.borrow().get_dir_sizes(threshold).unwrap();
+                    sizes.append(&mut more_sizes);
+                }
+            }
+        }
+
+        return Some(sizes);
+    }
 }
 
 fn main() {
-    let input = std::fs::read_to_string("./input/example_input.txt").expect("Invalid File");
+    let input = std::fs::read_to_string("./input/input.txt").expect("Invalid File");
 
-    let root = Rc::new(Directory {
-        name: "/".to_string(),
-        parent: None,
-        contents: RefCell::new(Vec::new()),
-    });
+    let root = Rc::new(RefCell::new(Directory::new(
+        "/".to_string(),
+        None,
+        Vec::new(),
+    )));
 
-    let mut current_directory: Rc<dyn FileSystemEntity> = root.clone();
+    let mut current_directory: Rc<RefCell<dyn FileSystemEntity>> = root.clone();
 
-    for raw_line in input.lines().skip(1) {
-        // let mut dir_contents = current_directory.get_contents().unwrap().borrow_mut();
-        let mut line = raw_line.split_whitespace();
-
-        match line.nth(0).unwrap() {
-            "$" => match line.nth(1).unwrap() {
-                "cd" => match line.nth(2).unwrap() {
-                    ".." => current_directory = current_directory.get_parent().unwrap().clone(),
+    for mut line in input.lines().skip(1).map(|x| x.split_whitespace()) {
+        match line.next().unwrap() {
+            "$" => match line.next().unwrap() {
+                "cd" => match line.next().unwrap() {
+                    ".." => {
+                        let parent = current_directory.borrow().get_parent().unwrap().clone();
+                        current_directory = parent;
+                    }
                     s => {
-                        current_directory = current_directory.get_contents().unwrap().borrow_mut()
-                        // dir_contents
-                            .iter()
-                            .find(|&d| d.is_dir() && d.get_name() == s)
-                            .unwrap()
-                            .clone()
+                        let child = current_directory.borrow().get_child(s).unwrap().clone();
+                        current_directory = child;
                     }
                 },
                 _ => (),
             },
             s => match s {
-                "dir" => current_directory.get_contents().unwrap().borrow_mut().push(Rc::new(Directory {
-                    name: line.nth(1).unwrap().to_string(),
-                    parent: Some(current_directory.clone()),
-                    contents: RefCell::new(Vec::new()),
-                })),
-                f => current_directory.get_contents().unwrap().borrow_mut().push(Rc::new(File {
-                    name: line.nth(1).unwrap().to_string(),
-                    parent: Some(current_directory.clone()),
-                    size: f.parse().unwrap(),
-                })),
+                "dir" => current_directory.borrow_mut().add_directory(Directory::new(
+                    line.next().unwrap().to_string(),
+                    Some(current_directory.clone()),
+                    Vec::new(),
+                )),
+                f => current_directory.borrow_mut().add_file(File::new(
+                    line.next().unwrap().to_string(),
+                    Some(current_directory.clone()),
+                    f.parse().unwrap(),
+                )),
             },
-        }
+        };
+    }
+
+    let disk_space: usize = 70000000;
+
+    let used_space: usize = root.borrow().get_size();
+
+    let free_space: usize = disk_space - used_space;
+
+    let required_space: usize = 30000000;
+
+    let dirs = root.borrow().get_dir_sizes(required_space - free_space).unwrap().iter().map(|d| d.1).min().unwrap();
+
+    println!("{:?}", dirs);
+
+
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    use crate::{Directory, FileSystemEntity};
+
+    #[test]
+    fn directory_if_parent_get_parent_returns_parent() {
+        let foo = Rc::new(RefCell::new(Directory::new(
+            "foo".to_string(),
+            None,
+            Vec::new(),
+        )));
+        let bar = Rc::new(RefCell::new(Directory::new(
+            "bar".to_string(),
+            Some(foo.clone()),
+            Vec::new(),
+        )));
+
+        bar.borrow().get_parent().unwrap();
+        assert_eq!(
+            bar.borrow().get_parent().unwrap().borrow().get_name(),
+            "foo"
+        );
+    }
+
+    #[test]
+    fn directory_add_directory() {
+        let foo = Rc::new(RefCell::new(Directory::new(
+            "foo".to_string(),
+            None,
+            Vec::new(),
+        )));
+        foo.borrow_mut().add_directory(Directory::new(
+            "bar".to_string(),
+            Some(foo.clone()),
+            Vec::new(),
+        ));
+    }
+
+    #[test]
+    fn current_directory_changes() {
+        let foo = Rc::new(RefCell::new(Directory::new(
+            "foo".to_string(),
+            None,
+            Vec::new(),
+        )));
+        let bar = Rc::new(RefCell::new(Directory::new(
+            "bar".to_string(),
+            Some(foo.clone()),
+            Vec::new(),
+        )));
+
+        let mut current_directory: Rc<RefCell<dyn FileSystemEntity>> = foo.clone();
+        assert_eq!(current_directory.borrow().get_name(), "foo");
+
+        current_directory = bar.clone();
+        assert_eq!(current_directory.borrow().get_name(), "bar");
+
+        let parent = current_directory.borrow().get_parent().unwrap().clone();
+        current_directory = parent;
+        assert_eq!(current_directory.borrow().get_name(), "foo");
     }
 }
